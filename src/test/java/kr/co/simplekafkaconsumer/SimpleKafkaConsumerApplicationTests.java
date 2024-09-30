c09ad4270a20a9c07daea4b3b45abc7455faa975
kidchang93 <lck930721@gmail.com>
2024-09-30 17:18:50 +0900

    Kafka Producer 와 연계되는 Consumer

---
+++ src/test/java/kr/co/simplekafkaconsumer/SimpleKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.simplekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
